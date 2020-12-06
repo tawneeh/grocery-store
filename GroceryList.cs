711556cd097ae4515bc2d81c1bfbecd23609a86a
Tawnee Harris <tawneeh@icloud.com>
2020-12-06 13:40:59 -0800

    add GroceryList class

---
+++ GroceryList.cs
@@ -0,0 +1,17 @@
+using System;
+
+class GroceryList
+{
+    static void Main()
+    {
+    string[] myGroceryList = {"eggs", "milk", "bread", "bananas", "cereal", "rice", "yogurt"};
+
+    Console.WriteLine("My grocery list:");
+
+    foreach (string groceryItem in myGroceryList)
+    {
+      Console.WriteLine(groceryItem);
+    }
+
+  }
+}
